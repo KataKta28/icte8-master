433bbc64849d748d3ece811c4aab95401ff3e568
Jonasgj <jgja12@student.aau.dk>
2016-05-14 15:03:00 +0200

    Branch test
    test

--- MAPP/app/src/main/java/com/icte8/mapp/mapp/SystemOverview.java
+++ MAPP/app/src/main/java/com/icte8/mapp/mapp/SystemOverview.java
@@ -4,11 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
 
 public class SystemOverview extends AppCompatActivity{
 
@@ -18,11 +17,11 @@ private View channelSetupButton;
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.system_overview);
-/*
+
         MongoClientURI uri = new MongoClientURI( "mongodb://username:password@www.example.com:12345/db-name" );
         MongoClient mongoClient = new MongoClient(uri);
         MongoDatabase db = mongoClient.getDatabase(uri.getDatabase());
-*/
+
 
 //List views for Gas
 /*
