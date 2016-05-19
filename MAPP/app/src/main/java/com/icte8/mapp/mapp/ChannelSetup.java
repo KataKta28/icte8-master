422337d3c37137235c03b6b6ff97d849afedce57
Jonasgj <jgja12@student.aau.dk>
2016-05-19 23:03:45 +0200

    Added saved thresholds
    Not currently distinguishing between detectors

--- MAPP/app/src/main/java/com/icte8/mapp/mapp/ChannelSetup.java
+++ MAPP/app/src/main/java/com/icte8/mapp/mapp/ChannelSetup.java
@@ -1,32 +1,43 @@
 package com.icte8.mapp.mapp;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class ChannelSetup extends AppCompatActivity {
 
     private View saveButton;
     private View cancelButton;
+
     @Override
 
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.channel_setup);
-
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
         final EditText setFilterHigh = (EditText) findViewById(R.id.inputFilterHigh);
-        final EditText  setGasHigh = (EditText) findViewById(R.id.inputGasAlarm);
+        final EditText setGasHigh = (EditText) findViewById(R.id.inputGasAlarm);
+        final EditText filterValue = (EditText) findViewById(R.id.filterHighValue);
+        final EditText gasValue = (EditText) findViewById(R.id.gasHighValue);
         saveButton = findViewById(R.id.saveSetup);
         cancelButton = findViewById(R.id.cancelSetup);
 
+        filterValue.setKeyListener(null);
+        gasValue.setKeyListener(null);
+
+        filterValue.setText(prefs.getInt("filter", 0) + "");
+        gasValue.setText(prefs.getInt("gas", 0) + "");
+
+
         cancelButton.setOnClickListener(
-                new View.OnClickListener(){
+                new View.OnClickListener() {
                     @Override
-                    public void onClick(View x){
+                    public void onClick(View x) {
                         finish();
                     }
                 }
@@ -37,32 +48,43 @@ public class ChannelSetup extends AppCompatActivity {
                     @Override
                     public void onClick(View V) {
 
-                        try{
-                            if (checkIfFilterEmpty(setFilterHigh.getText().toString().trim())){
+                        try {
+                            if (checkIfFilterEmpty(setFilterHigh.getText().toString().trim())) {
                                 setFilterHigh.setError("Value required");
                                 setFilterHigh.requestFocus();
-                            } else if (checkIfGasEmpty(setGasHigh.getText().toString().trim())){
-                                    setGasHigh.setError("Value required");
-                                    setGasHigh.requestFocus();
+                            } else if (checkIfGasEmpty(setGasHigh.getText().toString().trim())) {
+                                setGasHigh.setError("Value required");
+                                setGasHigh.requestFocus();
                             } else {
                                 //save shit to db
-                                finish();
+                                int filterSetValue = Integer.parseInt(setFilterHigh.getText().toString());
+                                int gasSetValue = Integer.parseInt(setGasHigh.getText().toString());
+
+                                filterValue.setText(filterSetValue + "");
+                                gasValue.setText(gasSetValue + "");
+
+                                savePreferences("filter", "gas", filterSetValue, gasSetValue);
+                                //savedValues(filterValue, gasValue);
+                                // finish();
                                 Toast.makeText(ChannelSetup.this, "Saved!", Toast.LENGTH_LONG).show();
                             }
+                        } catch (Exception x) {
+                            System.err.println(x.getClass().getName() + ": " + x.getMessage() + "The save has been unsuccessful");
                         }
-                        catch(Exception x){
-                            System.err.println( x.getClass().getName() + ": " + x.getMessage() + "The save has been unsuccessful");
-                        }
-                    }  } ); }
+                    }
+                }
+        );
+    }
 
-    protected boolean checkIfFilterEmpty (String x){
-      if (x.equals("") )
+    protected boolean checkIfFilterEmpty(String x) {
+        if (x.equals(""))
             return true;
         else
             return false;
 
-}
-    protected boolean checkIfGasEmpty (String y){
+    }
+
+    protected boolean checkIfGasEmpty(String y) {
         if (y.equals(""))
             return true;
         else
@@ -70,5 +92,12 @@ public class ChannelSetup extends AppCompatActivity {
 
     }
 
+    private void savePreferences(final String filter, final String gas, final int filterValue, final int gasValue) {
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(ChannelSetup.this);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(filter, filterValue);
+        editor.putInt(gas, gasValue);
+        editor.apply();
+    }
 }
 
