e89be5f2a9e1ba6834218b4d822476f41942e95b
KataKta28 <catalinpopescu07@gmail.com>
2016-06-21 15:39:44 +0200

    Latest

--- MAPP/app/src/main/java/com/icte8/mapp/mapp/MainActivity.java
+++ MAPP/app/src/main/java/com/icte8/mapp/mapp/MainActivity.java
@@ -6,6 +6,7 @@ import com.google.gson.JsonObject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,16 +25,12 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private View signIn;
-    private TextView result;
     private EditText username;
     private EditText password;
     private String pwd;
-
-
-    private static final Random RANDOM = new SecureRandom();
-    private static final int ITERATIONS = 10000;
-    private static final int KEY_LENGTH = 256;
-
+    public String response;
+    public  String [] temptoken;
+    public String token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,23 +41,18 @@ public class MainActivity extends AppCompatActivity {
         password = (EditText) findViewById(R.id.passwordInput);
 
         try { //establish connection to server
-
-             MongoClient mongoClient = new MongoClient("192.168.0.97", 8080);
+             MongoClient mongoClient = new MongoClient("192.168.0.53", 8080);
              MongoDatabase db = mongoClient.getDatabase("backend");
 
         } catch (Exception e) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() + "Check your connection");
         }
-
-
         signIn = findViewById(R.id.signinButton);
         signIn.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View V) {
                         try {
-
-
                             if (validateUsername(username.getText().toString().trim()) && (validatePassword(password.getText().toString().trim())))
                             {
                                 String usr = username.getText().toString().trim();
@@ -70,52 +62,41 @@ public class MainActivity extends AppCompatActivity {
                                 json.addProperty("name", usr);
                                 json.addProperty("password", pwd);
 
-
-
                                 Ion.with(MainActivity.this)
-                                        .load("http://192.168.0.97:8080/api/authenticate")
+                                        .load("http://192.168.0.53:8080/api/authenticate")
                                         .setJsonPojoBody(json)
                                         .asJsonObject()
                                         .withResponse()
                                         .setCallback(new FutureCallback<Response<JsonObject>>() {
                                             @Override
                                             public void onCompleted(Exception e, Response<JsonObject> result) {
-
-                                              //   Toast.makeText(MainActivity.this, result.toString(),Toast.LENGTH_LONG).show();
-                                            //    startActivity(new Intent(MainActivity.this, MainScreenActivity.class));
+                                            //   Toast.makeText(MainActivity.this, result.toString(),Toast.LENGTH_LONG).show();
+                                            //   startActivity(new Intent(MainActivity.this, MainScreenActivity.class));
                                                  if (result.getHeaders().code() == 404) {
                                                     Toast.makeText(MainActivity.this, "Incorrect Username or Password", Toast.LENGTH_LONG).show();
                                                 } else if (result.getHeaders().code() == 200) {
-                                                    startActivity(new Intent(MainActivity.this, MainScreenActivity.class));
+
+                                                     //  Toast.makeText(MainActivity.this, result.getResult().toString() ,Toast.LENGTH_LONG).show();
+                                                     response = result.getResult().toString();
+                                                     temptoken = response.split(" ");
+                                                     token = temptoken[1];
+                                                     Log.d("token", token);
+
+                                                     Intent intent = new Intent(MainActivity.this, MainScreenActivity.class);
+                                                     intent.putExtra("key", token);
+                                                     startActivity(intent);
                                                 }
                                             }
                                         });
-
-
-
-
-
-  /*
-
-
-                                Ion.with(MainActivity.this)
+  /*                            Ion.with(MainActivity.this)
                                         .load("http://192.168.0.97:8080/api/authenticate")
                                         .asString()
                                         .withResponse()
                                         .setCallback(new FutureCallback<Response<String>>() {
                                             @Override
                                             public void onCompleted(Exception e, Response<String> result) {
-
-
-
-
-                                            }
-
-                                        });*/
-
-
+                                            }});*/
                             }
-
                        else
                                 Toast.makeText(MainActivity.this, "Incorrect Username or Password", Toast.LENGTH_LONG).show();
                         } catch (Exception x) {
--- MAPP/app/src/main/java/com/icte8/mapp/mapp/MainScreenActivity.java
+++ MAPP/app/src/main/java/com/icte8/mapp/mapp/MainScreenActivity.java
@@ -3,7 +3,16 @@ package com.icte8.mapp.mapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.Response;
+
+import java.net.URL;
 
 public class MainScreenActivity extends AppCompatActivity {
 
@@ -11,6 +20,7 @@ public class MainScreenActivity extends AppCompatActivity {
     private View listAccounts;
     private View signOut;
     private View contact;
+    public String myToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -23,12 +33,44 @@ public class MainScreenActivity extends AppCompatActivity {
         signOut = findViewById(R.id.singOutButton);
         contact = findViewById(R.id.contactButton);
 
-
         overviewButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View V) {
-                        startActivity(new Intent(MainScreenActivity.this, SystemOverview.class));
+                        try {
+
+
+
+                            Intent intent= getIntent();
+                            String value = intent.getStringExtra("key");
+                            Log.d("2nd token", value);
+
+                        /*
+
+                              String myToken = getIntent().getStringExtra("key");
+                            Log.d("2nd token", myToken);
+
+
+                            JsonObject jsonToken = new JsonObject();
+                            jsonToken.addProperty("jwt", myToken);
+                            Ion.with(MainScreenActivity.this)
+                                    .load("http://192.168.0.97:8080/?????")
+                                    .setJsonPojoBody(jsonToken)
+                                    .asJsonObject()
+                                    .withResponse()
+                                    .setCallback(new FutureCallback<Response<JsonObject>>() {
+                                        @Override
+                                        public void onCompleted(Exception e, Response<JsonObject> result) {
+
+                                        }
+                                    });
+                    */
+
+
+                            startActivity(new Intent(MainScreenActivity.this, SystemOverview.class));
+                        } catch (Exception x) {
+                            Toast.makeText(MainScreenActivity.this, "Data cannot be retrieved", Toast.LENGTH_LONG).show();
+                        }
                     }
                 });
 
